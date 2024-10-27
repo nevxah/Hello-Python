e5d31edaaf8c789da2fa148799e5f4945761f176
♰ <145254289+nevxah@users.noreply.github.com>
2024-10-27 13:27:13 +0100

    cambios

---
+++ 00_hello.py
@@ -0,0 +1,25 @@
+# Esto es un comentario: Hola mundo
+# Nuestro hola mundo en python
+print("Hola python")
+print('Hola python')
+
+"""
+Este es un 
+comentario
+en multiples líneas
+"""
+
+'''
+Este también es un
+comentario
+en multiples líneas
+'''
+
+# Consultar el tipo de dato
+print(type("Soy un dato tipo str")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(1 + 3j)) # Tipo 'complex'
+print(type(True)) # Tipo 'bool'
+
+print(type(print("Mi cadena de texto"))) # Tipo 'NoneType'
---
+++ 01_variables.py
@@ -0,0 +1,21 @@
+# Variables
+
+my_string_variable = 'My str variable'
+print(my_string_variable)
+
+my_int_variable = 5
+print(my_int_variable) 
+
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+my_bool_variable = True 
+print(my_bool_variable) 
+
+# Concatenación de variables en un print
+print(my_string_variable, str(my_int_variable), my_bool_variable)
+print("Esta es el valor de:", my_bool_variable)
+
+# AlgunasFunciones del sistema
+print(len(my_string_variable))
