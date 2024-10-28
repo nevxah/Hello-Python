2864d3965351202090a118b5546a8005b9347375
♰ <145254289+nevxah@users.noreply.github.com>
2024-10-28 12:22:24 +0100

    cambios

--- 00_helloworld.py
+++ 00_helloworld.py
@@ -1,3 +1,5 @@
+### Hello-World ###
+
 # Esto es un comentario: Hola mundo
 # Nuestro hola mundo en python
 print("Hola python")
@@ -21,5 +23,4 @@ print(type(5)) # Tipo 'int'
 print(type(1.5)) # Tipo 'float'
 print(type(1 + 3j)) # Tipo 'complex'
 print(type(True)) # Tipo 'bool'
-
 print(type(print("Mi cadena de texto"))) # Tipo 'NoneType'
--- 01_variables.py
+++ 01_variables.py
@@ -1,4 +1,4 @@
-# Variables
+### Variables ###
 
 my_string_variable = 'My str variable'
 print(my_string_variable)
@@ -15,7 +15,32 @@ print(my_bool_variable)
 
 # Concatenación de variables en un print
 print(my_string_variable, str(my_int_variable), my_bool_variable)
-print("Esta es el valor de:", my_bool_variable)
+print("Este es el valor de:", my_bool_variable)
 
 # AlgunasFunciones del sistema
 print(len(my_string_variable))
+
+# Variables en una sola línea. ¡No abusar de esta sintaxis!
+name, surname, alias, age = "Pepe", "Perez", "pepito", 25
+print("Me llamo:", name, surname, "Y mi edad es:", age, "Y mi alias es:", alias)
+
+# Inputs
+"""
+name = input("¿Cual es tu nombre?: ")
+age = input("¿Cual es tu edad?: ")
+print(name)
+print(age)
+"""
+
+# Cambiamos su tipo
+name = 25
+age = "Pepe" 
+print(name)
+print(age)
+
+# ¿Forzamos el tipo?
+adrress: str = "Calle Falsa"
+adrress = 12
+adrress = True
+adrress = 12.5
+print(type(adrress))
